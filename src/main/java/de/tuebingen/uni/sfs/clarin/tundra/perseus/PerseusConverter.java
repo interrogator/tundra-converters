c914642ae32e022764757328b2207a9c63ab9b09
Alexandr Chernov <alexandr.chernov@uni-tuebingen.de>
2016-02-19 15:23:58 +0100

    Added a class for conversion from the Perseues format into the Tuendra format

---
+++ src/main/java/de/tuebingen/uni/sfs/clarin/tundra/perseus/PerseusConverter.java
@@ -0,0 +1,10 @@
+package de.tuebingen.uni.sfs.clarin.tundra.perseus;
+
+/**
+ * Created by megalex on 19/02/16.
+ */
+public class PerseusConverter {
+    public void convert(String inputString) {
+        System.out.println(inputString);
+    }
+}
